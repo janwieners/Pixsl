76e985a9266ca40120d8e35c1058b6eae23c99b9
Jan G. Wieners <jan@jan-wieners.de>
2020-09-03 01:02:21 +0200

    Display text via div.

--- src/app/components/pixslspace/pixslspace.component.ts
+++ src/app/components/pixslspace/pixslspace.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from 'src/app/services/data.service';
+
+declare var randomColor: any;
+
+export interface IColorHash {
+  [symbol: string]: string;
+}
 
 @Component({
   selector: 'app-pixslspace',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PixslspaceComponent implements OnInit {
 
-  constructor() { }
+  private colors: string[];
+  public colorCodes: IColorHash = {};
+  public text: string[];
+  private unique: string[];
+
+  constructor(public data: DataService) {
+  }
 
   ngOnInit(): void {
+    this.data.dataUpdated.subscribe(() => {
+        this.text = this.data.getProcessedText();
+        this.unique = this.data.getUniqueChars();
+
+        this.colors = randomColor({
+          count: this.unique.length
+        });
+
+        let i = 0;
+        this.unique.forEach((char) => {
+          this.colorCodes[char] = this.colors[i++];
+        });
+      }
+    );
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
@@ -6,16 +6,48 @@ import {HttpClient} from '@angular/common/http';
 })
 export class DataService {
 
+  private rawText: string;
+  private processedText: string[];
+  private uniqueChars: string[];
+  public dataUpdated = new EventEmitter();
+
   constructor(private http: HttpClient) {
+  }
+
+  private unique(input: string): string[] {
+    return input.split('')
+      .filter((n, i) => input.indexOf(n) === i);
+  }
+
+  private cleanup(input: string): string {
+    return input.replace(/(\r\n|\n|\r)/gm, '');
+    // return input.replace(/(\r\n|\n|\s|\r)/gm, '');
   }
 
   public loadTextFile(url: string): Promise<any> {
     return new Promise<string>((resolve, reject) => {
       this.http.get(url, {responseType: 'text'}).subscribe(data => {
+        this.rawText = data;
+        this.processedText = this.cleanup(data).split('');
+        this.uniqueChars = this.unique(this.cleanup(data));
+        this.dataUpdated.emit(data);
         resolve(data);
       }, error => {
         reject(error);
       });
     });
   }
+
+  public getRawText(): string {
+    return this.rawText;
+  }
+
+  public getProcessedText(): string[] {
+    return this.processedText;
+  }
+
+  public getUniqueChars(): string[] {
+    return this.uniqueChars;
+  }
+
 }
